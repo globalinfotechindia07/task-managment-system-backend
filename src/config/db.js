770732216d2e539c83f55915174859451cf07127
Dinesh Tawade <dineshaiits@gmail.com>
2026-07-21 12:09:00 +0530

    update database connection string

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/company_management');
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://mansibsharma10_db_user:global123@cluster1.dlp0uvg.mongodb.net/task-management-system?retryWrites=true&w=majority&appName=Cluster1');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
